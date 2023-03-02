fe67452afac242a4dd95e82e7a3ce1d4ecbe14a3
Edward Zota <zotaedward@gmail.com>
2023-03-02 14:16:22 +0100

    esercizio Palindromi concluso

---
+++ Palidroma/js/myscript.js
@@ -0,0 +1,26 @@
+const scegliParola = prompt('Inserisci una parola o una frase a tua scelta e ti dirò se è palindroma oppure no.');
+
+console.log(scegliParola);
+
+console.log(testPalindromo(scegliParola));
+
+
+
+// funzioni 
+
+function testPalindromo (Parola){
+
+    lunghezzaParola = Parola.length
+
+    for(let i = 0; i < lunghezzaParola / 2; i++){
+
+        if(Parola[i] !== Parola[lunghezzaParola - 1 - i]) {
+
+            return `La parola da te scelta NON è palindroma.`;
+        }
+
+    }
+
+    return `La parola da te scelta È palindroma.`;
+
+}
